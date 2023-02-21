c14b933a99cda14d56252f87f1c973fa72e10d9f
Sberrich <darksamir2012@gmail.com>
2023-02-21 17:02:21 +0100

    create the navbar and search components

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Navbar, Feed, PinDetails, CreatePin, Search } from "../components";
 
-const Pins = () => {
-  return <div>Pins</div>;
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="px-2 md:px-5 ">
+      <div className="bg-gray-50">
+        <Navbar
+          searchTerm={searchTerm}
+          user={user}
+          setSearchTerm={setSearchTerm}
+        />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category:categoryId" element={<Feed />} />
+          <Route
+            path="/pin-detail/:pinId"
+            element={<PinDetails user={user} />}
+          />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route
+            path="/search"
+            element={
+              <Search setSearchTerm={setSearchTerm} searchTerm={searchTerm} />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default Pins;
