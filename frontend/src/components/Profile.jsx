c0321a0f8f773c77e5aa33a0d4311961099fe627
Sberrich <darksamir2012@gmail.com>
2023-02-21 16:24:01 +0100

    Finish the sidebar

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>profile</div>;
+};
 
-export default Profile
+export default Profile;
--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Pins = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>Pins</div>;
+};
 
-export default Pins
+export default Pins;
